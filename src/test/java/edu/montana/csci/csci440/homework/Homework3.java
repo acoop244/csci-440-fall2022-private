8556e151b635adf65fd438060e84f7e8aca8a49d
Austin Coop <acoop244@gmail.com>
2022-12-02 21:25:45 -0700

    Finished questions 2 and 3 of Homework3.java

--- src/test/java/edu/montana/csci/csci440/homework/Homework3.java
+++ src/test/java/edu/montana/csci/csci440/homework/Homework3.java
@@ -57,7 +57,7 @@ public class Homework3 extends DBTest {
     /*
      * Select tracks that have been sold more than once (> 1)
      *
-     * Select the albumbs that have tracks that have been sold more than once (> 1)
+     * Select the albums that have tracks that have been sold more than once (> 1)
      *   NOTE: This is NOT the same as albums whose tracks have been sold more than once!
      *         An album could have had three tracks, each sold once, and should not be included
      *         in this result.  It should only include the albums of the tracks found in the first
@@ -66,12 +66,21 @@ public class Homework3 extends DBTest {
     public void selectPopularTracksAndTheirAlbums() throws SQLException {
 
         // HINT: join to invoice items and do a group by/having to get the right answer
-        List<Map<String, Object>> tracks = executeSQL("");
+        List<Map<String, Object>> tracks = executeSQL("SELECT *, count(invoice_items.TrackId) as TrackCount\n" +
+                "FROM tracks\n" +
+                "JOIN invoice_items on tracks.TrackId = invoice_items.TrackId\n" +
+                "GROUP BY invoice_items.TrackId\n" +
+                "HAVING TrackCount > 1");
         assertEquals(256, tracks.size());
 
         // HINT: join to tracks and invoice items and do a group by/having to get the right answer
         //       note: you will need to use the DISTINCT operator to get the right result!
-        List<Map<String, Object>> albums = executeSQL("");
+        List<Map<String, Object>> albums = executeSQL("SELECT DISTINCT albums.Title as AlbumTitle, count(invoice_items.TrackId) as TrackCount\n" +
+                "FROM albums\n" +
+                "JOIN tracks on albums.AlbumId = tracks.AlbumId\n" +
+                "JOIN invoice_items on tracks.TrackId = invoice_items.TrackId\n" +
+                "GROUP BY invoice_items.TrackId\n" +
+                "HAVING TrackCount > 1");
         assertEquals(166, albums.size());
     }
 
@@ -84,7 +93,16 @@ public class Homework3 extends DBTest {
      * */
     public void selectCustomersMeetingCriteria() throws SQLException {
         // HINT: join to invoice items and do a group by/having to get the right answer
-        List<Map<String, Object>> tracks = executeSQL("" );
+        List<Map<String, Object>> tracks = executeSQL("SELECT customers.Email, employees.FirstName\n" +
+                "FROM customers\n" +
+                "JOIN employees on customers.SupportRepId = employees.EmployeeId\n" +
+                "WHERE employees.EmployeeId = 3\n" +
+                "AND customers.CustomerId IN (SELECT invoices.CustomerId\n" +
+                "                             FROM tracks\n" +
+                "                                      JOIN genres on tracks.GenreId = genres.GenreId\n" +
+                "                                      JOIN invoice_items on tracks.TrackId = invoice_items.TrackId\n" +
+                "                                      JOIN invoices on invoice_items.InvoiceId = invoices.InvoiceId\n" +
+                "                             WHERE genres.Name = \"Rock\")" );
         assertEquals(21, tracks.size());
     }
 
