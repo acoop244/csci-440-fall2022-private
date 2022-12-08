037202816d161fdf52550e0d3d5b6b9f8b5fc217
Austin Coop <acoop244@gmail.com>
2022-12-07 20:47:51 -0700

    finished :)

--- src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
+++ src/main/java/edu/montana/csci/csci440/helpers/EmployeeHelper.java
@@ -3,6 +3,7 @@ package edu.montana.csci.csci440.helpers;
 import edu.montana.csci.csci440.model.Employee;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
@@ -12,7 +13,17 @@ public class EmployeeHelper {
         // TODO, change this to use a single query operation to get all employees
         Employee employee = Employee.find(1); // root employee
         // and use this data structure to maintain reference information needed to build the tree structure
+        List<Employee> all = Employee.all();
         Map<Long, List<Employee>> employeeMap = new HashMap<>();
+
+        employeeMap.put(employee.getEmployeeId(), new LinkedList<>());
+        for(Employee current : all){
+            Long boss = current.getReportsTo();
+            if(employeeMap.containsKey(boss)) {
+                employeeMap.get(boss).add(current);
+            }
+            employeeMap.put(current.getEmployeeId(), new LinkedList<>());
+        }
         return "<ul>" + makeTree(employee, employeeMap) + "</ul>";
     }
 
@@ -21,7 +32,7 @@ public class EmployeeHelper {
     public static String makeTree(Employee employee, Map<Long, List<Employee>> employeeMap) {
         String list = "<li><a href='/employees" + employee.getEmployeeId() + "'>"
                 + employee.getEmail() + "</a><ul>";
-        List<Employee> reports = employee.getReports();
+        List<Employee> reports = employeeMap.get(employee.getEmployeeId());
         for (Employee report : reports) {
             list += makeTree(report, employeeMap);
         }
